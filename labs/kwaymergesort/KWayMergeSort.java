b2f8d14681710fc0f90cfae98a1a4270124f470b
Kate Harline <kharline@wustl.edu>
2016-10-26 22:36:50 -0500

    return value?

--- labs/kwaymergesort/KWayMergeSort.java
+++ labs/kwaymergesort/KWayMergeSort.java
@@ -13,6 +13,8 @@ public class KWayMergeSort {
 	 */
 	public static Integer[] kwaymergesort(int K, Integer[] input, Ticker ticker) {
 		int n = input.length;
+		//allocate array to keep track of split pieces
+		ticker.tick(n);
 		Integer[][] kSplit = new Integer[K][n/K];
 		
 
@@ -29,28 +31,33 @@ public class KWayMergeSort {
 		
 		//if n=1 or n=0, "trivially sort"
 		if(n == 1){
+			ticker.tick();
 			Integer[] ans = new Integer[n*2];
 			//merge karrays back together
 			//go through the karrays that exist
-			for(int i=0; i<n*2; i=i+2){
+			for(int i=0; i<n*2; ++i){ 
+				ticker.tick();
+				int amove = 0;
+				int bmove = 0;
 				//find the pair elements and compare
-				if(kSplit[i][i] < kSplit[i+1][i]){
-					ans[i] = kSplit[i][i];
+				if(kSplit[i][amove] <= kSplit[i+2][bmove]){
+					ticker.tick();
+					ans[i] = kSplit[i][amove];
+					++amove;
 				}
 				else{
-					ans[i] =kSplit[i+1][i];
-					ans[i+1]=kSplit[i][i];
+					ticker.tick();
+					ans[i] =kSplit[n/2+1][bmove];
+					++bmove;
 				}
 			}
-			
+			return ans;
 		}
 		//otherwise divide into subcollections
 		//recursively sort subcollections, merge and sort the subcollections
 		//this is a more general sort, n/k k subcollections
 		else{
-			System.out.println("hello world");
-			//allocate array to keep track of split pieces
-	
+			
 			ticker.tick();
 					//assign k arrays to split, keep track of where splits are with double array
 					for(int i=0; i<K; i++){
@@ -59,26 +66,19 @@ public class KWayMergeSort {
 							ticker.tick();
 							System.out.println("this is the input " + input[j+(i*n/K)]);
 							//System.out.println(j+(i*n/K));
-							//recursively calls mergesort on these smaller arrays
+							ticker.tick();
 							kSplit[i][j] = input[j+(i*n/K)];
 							//returns an integer, capture this integer
 							System.out.println(""+kSplit[i].length);
 						}
-						//recursively call splitting
-						kwaymergesort(K, kSplit[i], ticker);
+						ticker.tick();
+						//recursively calls mergesort on these smaller arrays
+						return kwaymergesort(K, kSplit[i], ticker);
 					}
 		}
 		//compare lowest nodes and merge
 		
 		//eventually assign to array containing all inputs
-		return ans;
 	}
 	
-//	public static Integer[] merge(Integer[] input1, Integer[] input2, int K, Ticker ticker){
-//		Integer[] ans = new Integer[2*input1.length];
-//		
-//		
-//		
-//		return ans;
-//	}
 }
