fc2d5582b9970730f3fce1b3fc74ad3ed41b8006
Kate Harline <kharline@wustl.edu>
2016-11-10 21:30:57 -0600

    breaks in the middle

--- labs/rabinkarp/RK.java
+++ labs/rabinkarp/RK.java
@@ -2,18 +2,13 @@ package rabinkarp;
 
 public class RK {
 	//a rk has-a window and, string of characters, start
-	public int window, numCalls, prevHash;
+	public int window, numCalls, prevHash, exp;
 	public char[] prevChars;
 	//
 	// Be sure to look at the write up for this assignment
 	//  so that you get full credit by satisfying all
 	//  of its requirements
-	//target is string of length s
-	//query is string of length q
-	//find all occurences of q in s
-	//rolling hash computed, so don't match q in all of s, window is q
-	//where there aren't q characters in the window, 0 is used
-	//first character of window q at index start (start of window) 
+
 	/**
 	 * Rabin-Karp string matching for a window of the specified size, constructor?
 	 * @param m size of the window
@@ -23,6 +18,11 @@ public class RK {
 		numCalls = 0;
 		prevChars = new char[m];
 		prevHash = 0;
+		//exponentiation should only have to do this once for time complexity
+		exp = 1;
+		for(int i=0; i<window; i++){
+			this.exp = (exp*31)%511;
+		}
 	}
 
 	/**
@@ -31,20 +31,20 @@ public class RK {
 	 * @return the new hash, with char d added and char m-1 subtracted
 	 */
 	public int nextCh(char d) {
+		//rolling hash is rolling assignment of prev chars to a char array that you maintain, calc oldHash and then undo/update
+		char byeChar = prevChars[numCalls % window];
+		//reassign char to where char that just left was
+		prevChars[numCalls % window] = d;
+		++numCalls;
 		//calculate hash of the character based on hashfor calculation
-		//exponentiation
-		int exp = 1;
-		for(int i=0; i<window; i++){
-			exp = (exp*31)%511;
+		int byebye = 0;
+		//when you need to start removing chars, otherwise subtract zero in the beginning
+		if(numCalls > window){
+			byebye = exp * (byeChar%511) % 511;
 		}
 		//somehow calculate this
 		//h = (h×31 – 31m×c + d) mod 511
-		//rolling hash is rolling assignment of prev chars to a char array that you maintain, calc oldHash and then undo/update
-		this.prevHash =  ((31*prevHash % 511 - ((exp * prevChars[numCalls % window])%511)%511 + d % 511) % 511);
-		//reassign char to its place in the 
-		prevChars[numCalls % window] = d;
-		++numCalls;
-		
+		this.prevHash =  ((31*prevHash % 511 - byebye + d % 511) % 511);
 		return prevHash;
 	}
 	
